9f7e8c73c428c1c6bc937cd830bd9717c03ec393
Harry Brundage <harry.brundage@gmail.com>
2018-07-16 16:54:12 -0400

    WIP

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export IP=$(ipconfig getifaddr en0)
 export TURTLEBOT_IP=$(resin device 851079b | grep "IP ADDRESS" | awk '{ print $3 }')
-docker-compose "$@"
+docker-compose -f docker-compose.simulation.yml "$@"
--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 set -ex
 defaults write org.macosforge.xquartz.X11 app_to_run '' # suppress xterm terminal
 defaults write org.macosforge.xquartz.X11 enable_iglx -bool true
+
 open -a XQuartz
 /opt/X11/bin/xhost +$(ipconfig getifaddr en0)
 
 brew tap osrf/simulation
-brew install gazebo9
+brew install gazebo7
